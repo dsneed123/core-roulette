952a7fb0709d77a1673b31405bef18ee0b335bc5
dsneed123 <101902819+dsneed123@users.noreply.github.com>
2024-10-23 02:48:32 -0700

    fix intermediate file

--- src/pages/workout.jsx
+++ src/pages/workout.jsx
@@ -3,7 +3,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import 'app/globals.css';
+import '../app/globals.css';
 import Back from '../components/back';
 
 const Workout = () => {
